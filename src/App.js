49c1b01234c85c7043e1c22a48781c48d78ef902
artemis dev <romio1990z@gmail.com>
2024-04-27 18:06:54 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+function App() {
+  return (
+    <>
+      <div id="profile">
+        <img id="userPhoto" src="images/profile.png" alt="ธนะบอมฐ์ โชควรเตชะทรัพย์'s Profile Picture" />
+          <div id="userName">
+            <h1 id="usersName">ธนะบอมฐ์ โชควรเตชะทรัพย์</h1>
+            <p id="usersProfession">ช่องทางการติดต่อ</p>
+          </div>
+      </div>
+      <div id="links">
+        <a className="link" href="https://www.facebook.com/UnC789" target="_blank" rel="noopener" id="linkOne">
+          <i className="fa-brands fa-facebook" id="iconOne"></i> Facebook
+        </a>
+        <a className="link" href="https://www.instagram.com/ig_bjm/" target="_blank" rel="noopener" id="linkTwo">
+          <i className="fa-brands fa-instagram" id="iconTwo"></i> Instagram
+        </a>
+        <a className="link" href="https://www.xn--92cwj1abdoj9lxae3cwgf6o.com/" target="_blank" rel="noopener" id="linkThree">
+          <i className="fa fa-globe" id="iconThree"></i> ฉีดโฟมพียูทำแพ.com
+        </a>
+        <a className="link" href="http://www.thanabom.com/" target="_blank" rel="noopener" id="linkThree">
+          <i className="fa fa-globe" id="iconThree"></i> Thanabom.com
+        </a>
+        <a className="link" href="mailto:jokmok5@gmail.com" target="_blank" rel="noopener" id="linkFour">
+          <i className="fa fa-envelope" id="iconFour"></i> Email
+        </a>
+        <a className="link" href="https://line.me/ti/p/~TG7989" target="_blank" rel="noopener" id="linkFive">
+          <i className="fa-brands fa-line" id="iconFive"></i> Line
+        </a>
+        <a className="link" href="https://www.tiktok.com/@pufoam897?_t=8lsJQ598FPE&_r=1" target="_blank" rel="noopener" id="linkFive">
+          <i className="fa-brands fa-tiktok" id="iconFive"></i> Tiktok
+        </a>
+        <a className="link" href="https://www.youtube.com/@JOKMOKTV" target="_blank" rel="noopener" id="linkFive">
+          <i className="fa-brands fa-youtube" id="iconFive"></i> JokmokoTV
+        </a>
+        <a className="link" href="https://twitter.com/djbomjokmok" target="_blank" rel="noopener" id="linkFive">
+          <i className="fa-brands fa-twitter" id="iconFive"></i> twitter
+        </a>
+        <a className="link" href="tel:061-897-8979" target="_blank" rel="noopener" id="linkSix">
+          <i className="fa fa-phone" id="iconSix"></i> เบอร์โทร
+        </a>
+      </div>
+      <p id="credit">
+        <a rel="noopener" href="https://www.%E0%B8%89%E0%B8%B5%E0%B8%94%E0%B9%82%E0%B8%9F%E0%B8%A1%E0%B8%9E%E0%B8%B5%E0%B8%A2%E0%B8%B9%E0%B8%97%E0%B9%8D%E0%B8%B2%E0%B9%81%E0%B8%9E.com/" target="_blank">บริษัท ธนะบอมฐ์ คอนทรัคชั่น จำกัด</a>
+      </p>
+    
+    </>
+  );
+}
+
+export default App;
